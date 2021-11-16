b31a2589f7b993c86fa2058e87ca64bd24d94bd5
himvais <himvais@hotmail.com>
2021-11-16 18:30:00 +0530

    Bringing in the updated version from Developers

---
+++ environments/localhost.js
@@ -0,0 +1,10 @@
+window['env'] = {
+    production: false,
+    urls: {
+      apiUrl: 'https://erm-esg-web-api-dev.azurewebsites.net/',        
+      clientLoginUrl: 'https://erm-esg-ui-dev.azurewebsites.net',
+      videoUrl: 'https://eusrgdesg01storage.blob.core.windows.net/microsite-video/erm.mp4',
+      powerBIReportUrl: 'https://app.powerbi.com/view?r=eyJrIjoiODExMzBhMzEtN2E1NS00MzNkLWFjYzMtYmI3NGQ1YzdhMGNhIiwidCI6ImI5ZDJlMjMwLWZiNTktNDNhNS1hZDMxLTAxNGI2OGQ3YWZkZiJ9&pageName=ReportSectiona9f71e51080208c83851'
+    }
+}
+localStorage.setItem('env', JSON.stringify(window['env']));
